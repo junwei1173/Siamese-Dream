55d485b6c683a45cee324c7d3ffe138ba9fa6064
Junwei Chen <149391722+junwei1173@users.noreply.github.com>
2025-08-23 23:21:25 -0400

    Update DreamAnalysis.jsx

--- siamesedream-frontend/src/components/DreamAnalysis.jsx
+++ siamesedream-frontend/src/components/DreamAnalysis.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect, useContext, useCallback } from 'react';
 import { Link } from 'react-router-dom';
 import { AuthContext } from '../AuthContext';
+import API_URL from '../config/api';
 
 function DreamAnalysis() {
   const [analysisData, setAnalysisData] = useState(null);
