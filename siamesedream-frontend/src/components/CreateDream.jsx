83c1a861063c144241d8d9985896133f5fba4184
Junwei Chen <149391722+junwei1173@users.noreply.github.com>
2025-08-23 23:20:57 -0400

    Update CreateDream.jsx

--- siamesedream-frontend/src/components/CreateDream.jsx
+++ siamesedream-frontend/src/components/CreateDream.jsx
@@ -2,6 +2,7 @@ import { useState, useContext } from "react";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
 import { AuthContext } from "../AuthContext";
+import API_URL from '../config/api';
 
 function CreateDream() {
   const { user } = useContext(AuthContext);
