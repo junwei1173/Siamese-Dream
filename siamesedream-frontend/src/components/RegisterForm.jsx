ca56905e0658dd40e89038868aef0849eaee6ef3
Junwei Chen <149391722+junwei1173@users.noreply.github.com>
2025-08-23 23:22:04 -0400

    Update RegisterForm.jsx

--- siamesedream-frontend/src/components/RegisterForm.jsx
+++ siamesedream-frontend/src/components/RegisterForm.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import axios from "axios";
 import { Link } from "react-router-dom";
+import API_URL from '../config/api';
 
 function RegisterForm() {
   const [form, setForm] = useState({ username: "", email: "", password: "" });
